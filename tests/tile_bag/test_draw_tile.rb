a7dae11862571745f55d80d4b789b2b0f850287b
jmurrel1 <jmurrel1@cs6242-vm04.uwg.westga.edu>
2022-01-18 17:08:53 -0500

    test draw tile ready to test

--- tests/tile_bag/test_draw_tile.rb
+++ tests/tile_bag/test_draw_tile.rb
@@ -3,12 +3,46 @@ require_relative "../../tile_bag.rb"
 
 class TileBag::TestDrawTile < MiniTest::Test
 
+	def setup
+		@tile_bag = TileBag.new
+	end
+
 	def test_has_proper_number_of_tiles
-		skip "Test not yet implemented"
+		(1..98).each do |i|
+
+			assert_equal false, @tile_bag.empty?
+			@tile_bag.pop
+		end
+		assert_equal true, @tile_bag.empty?
 	end
 
 	def test_has_proper_tile_distribution
-		skip "Test not yet implemented"
+		@current = {
+			Q: 0, Z: 0,
+			J: 0, X: 0,
+			K: 0,
+			F: 0, H: 0, V: 0, W: 0, Y: 0,
+			B: 0, C: 0, M: 0, P: 0,
+			D: 0, G: 0,
+			E: 0, A: 0, I: 0, O: 0, N: 0, R: 0, T: 0, L: 0, S: 0, U: 0
+		}
+
+		@expected = {
+			Q: 1, Z: 1,
+			J: 1, X: 1,
+			K: 1,
+			F: 2, H: 2, V: 2, W: 2, Y: 2,
+			B: 2, C: 2, M: 2, P: 2,
+			D: 4, G: 3,
+			E: 12, A: 9, I: 9, O: 8, N: 6, R: 6, T: 6, L: 4, S: 4, U: 4
+		}
+
+		until @tile_bag.empty?
+			@tile = @tile_bag.pop
+			@current[@tile] += 1
+		end
+
+		assert_equal @expected, @current
 	end
 
 end
