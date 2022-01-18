84be3cc6e2e84ffe12f7d5ff9f189fdc162499bc
jmurrel1 <jmurrel1@cs6242-vm04.uwg.westga.edu>
2022-01-18 17:45:17 -0500

    tilegroup::remove is tested

--- tests/tile_group/test_remove.rb
+++ tests/tile_group/test_remove.rb
@@ -5,30 +5,40 @@ class TileGroup::TestRemove < MiniTest::Test
 
 	def setup
 		@tile_group = TileGroup.new
+		@tile_group.tiles = [:A, :B, :C]
 	end
 
 	def test_remove_only_tile
-		skip "Test not yet implemented"
+		@tile_group.tiles = [:A]
+		@tile_group.remove(:A)
+		assert_equal true, @tile_group.tiles.empty?
 	end
 
 	def test_remove_first_tile_from_many
-		skip "Test not yet implemented"
+		@tile_group.remove(:A)
+		assert_equal [:B, :C], @tile_group.tiles
 	end
 
 	def test_remove_last_tile_from_many
-		skip "Test not yet implemented"
+		@tile_group.remove(:C)
+		assert_equal [:A, :B], @tile_group.tiles
 	end
 
 	def test_remove_middle_tile_from_many
-		skip "Test not yet implemented"
+		@tile_group.remove(:B)
+		assert_equal [:A, :C], @tile_group.tiles
 	end
 
 	def test_remove_multiple_tiles
-		skip "Test not yet implemented"
+		@tile_group.remove(:B)
+		@tile_group.remove(:A)
+		assert_equal [:C], @tile_group.tiles
 	end
 
 	def test_make_sure_duplicates_are_not_removed
-		skip "Test not yet implemented"
+		@tile_group.tiles = [:A, :B, :C, :A, :A]
+		@tile_group.remove(:A)
+		assert_equal [:B, :C, :A, :A], @tile_group.tiles
 	end
 
 end
