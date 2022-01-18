52a2869cbc2f61714d09c5ac825c5c4407598967
jmurrel1 <jmurrel1@cs6242-vm04.uwg.westga.edu>
2022-01-18 17:29:47 -0500

    word.rb

--- word.rb
+++ word.rb
@@ -1,9 +1,14 @@
+require_relative "tile_bag.rb"
+
 class Word < TileGroup
-	
+
 	def initialize()
 	end
-	
+
 	def score
+		@score = 0
+		@tiles.each {|tile| @score += TileBag.points_for(tile)}
+		@score
 	end
 
 end
