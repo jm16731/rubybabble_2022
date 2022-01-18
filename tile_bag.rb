eefda514580ee8e91ffccb15bad59b4807009d04
jmurrel1 <jmurrel1@cs6242-vm04.uwg.westga.edu>
2022-01-18 16:53:58 -0500

    class variable

--- tile_bag.rb
+++ tile_bag.rb
@@ -1,5 +1,15 @@
 class TileBag
 
+	@@point_values = {
+		Q: 10, Z: 10,
+		J: 8, X: 8,
+		K: 5,
+		F: 4, H: 4, V: 4, W: 4, Y: 4,
+		B: 3, C: 3, M: 3, P: 3,
+		D: 2, G: 2,
+		E: 1, A: 1, I: 1, O: 1, N: 1, R: 1, T: 1, L: 1, S: 1, U: 1
+	}
+
 	def initialize
 			@tile_bag = [
 				:Q, :Z, :J, :K, :X,
@@ -15,17 +25,6 @@ class TileBag
 				:A, :A, :A, :A, :A, :A, :A, :A, :A,
 				:E, :E, :E, :E, :E, :E, :E, :E, :E, :E, :E, :E
 			]
-
-			@point_values = {
-				Q: 10, Z: 10,
-				J: 8, X: 8,
-				K: 5,
-				F: 4, H: 4, V: 4, W: 4, Y: 4,
-				B: 3, C: 3, M: 3, P: 3,
-				D: 2, G: 2,
-				E: 1, A: 1, I: 1, O: 1, N: 1, R: 1, T: 1, L: 1, S: 1, U: 1
-			}
-
 			@tile_bag.shuffle!
 	end
 
@@ -38,6 +37,6 @@ class TileBag
 	end
 
 	def self.points_for(tile)
-		@point_values[tile]
+		@@point_values[tile]
 	end
 end
