b6086ddd80b9a479dc3e8ff8fe4c0387b0596d5a
jmurrel1 <jmurrel1@cs6242-vm13.uwg.westga.edu>
2022-01-20 16:40:35 -0500

    test_hand ready to test

--- tests/tile_group/test_hand.rb
+++ tests/tile_group/test_hand.rb
@@ -8,19 +8,26 @@ class TileGroup::TestHand < MiniTest::Test
   end
 
   def test_convert_empty_group_to_string
-    skip "Test not yet implemented"
+    assert_equal "", @tile_group.hand
   end
 
   def test_convert_single_tile_group_to_string
-    skip "Test not yet implemented"
+    @tile_group.append(:A)
+    assert_equal "A", @tile_group.hand
   end
 
   def test_convert_multi_tile_group_to_string
-    skip "Test not yet implemented"
+    @tile_group.append(:A)
+    @tile_group.append(:B)
+    @tile_group.append(:C)
+    assert_equal "ABC", @tile_group.hand
   end
 
   def test_convert_multi_tile_group_with_duplicates_to_string
-    skip "Test not yet implemented"
+    @tile_group.append(:A)
+    @tile_group.append(:A)
+    @tile_group.append(:A)
+    assert_equal "AAA", @tile_group.hand
   end
 
 end
