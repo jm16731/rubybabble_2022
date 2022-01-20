f8bbe18cdeb6151ee056af7c18dc2f7d7cc865d3
jmurrel1 <jmurrel1@cs6242-vm13.uwg.westga.edu>
2022-01-20 17:15:42 -0500

    Word::TestScore ready to test

--- tests/word/test_score.rb
+++ tests/word/test_score.rb
@@ -8,19 +8,29 @@ class Word::TestScore < MiniTest::Test
   end
 
   def test_empty_word_should_have_score_of_zero
-    skip "Test not yet implemented"
+    assert_equal 0, @word.score
   end
 
   def test_score_a_one_tile_word
-    skip "Test not yet implemented"
+    @word.append(:A)
+    assert_equal 1, @word.score
   end
 
   def test_score_a_word_with_multiple_different_tiles
-    skip "Test not yet implemented"
+    @word.append(:C)
+    @word.append(:A)
+    @word.append(:T)
+    assert_equal 5, @word.score
   end
 
   def test_score_a_word_with_recurring_tiles
-    skip "Test not yet implemented"
+    @word.append(:M)
+    @word.append(:A)
+    @word.append(:R)
+    @word.append(:R)
+    @word.append(:O)
+    @word.append(:W)
+    assert_equal 11, @word.score
   end
 
 end
