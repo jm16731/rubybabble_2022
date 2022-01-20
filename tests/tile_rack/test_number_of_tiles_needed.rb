e6a81211a854dc727d640013394bd431605d0f58
jmurrel1 <jmurrel1@cs6242-vm13.uwg.westga.edu>
2022-01-20 17:22:53 -0500

    TileRack::NumberOfTilesNeeded written and tested

--- tests/tile_rack/test_number_of_tiles_needed.rb
+++ tests/tile_rack/test_number_of_tiles_needed.rb
@@ -8,19 +8,30 @@ class TileRack::TestNumberOfTilesNeeded < MiniTest::Test
   end
 
   def test_empty_tile_rack_should_need_max_tiles
-    skip "Test not yet implemented"
+    assert_equal 7, @tile_rack.number_of_tiles_needed
   end
 
   def test_tile_rack_with_one_tile_should_need_max_minus_one_tiles
-    skip "Test not yet implemented"
+    @tile_rack.append(:A)
+    assert_equal 6, @tile_rack.number_of_tiles_needed
   end
 
   def test_tile_rack_with_several_tiles_should_need_some_tiles
-    skip "Test not yet implemented"
+    @tile_rack.append(:A)
+    @tile_rack.append(:B)
+    @tile_rack.append(:C)
+    assert_equal 4, @tile_rack.number_of_tiles_needed
   end
 
   def test_that_full_tile_rack_doesnt_need_any_tiles
-    skip "Test not yet implemented"
+    @tile_rack.append(:A)
+    @tile_rack.append(:B)
+    @tile_rack.append(:C)
+    @tile_rack.append(:A)
+    @tile_rack.append(:B)
+    @tile_rack.append(:C)
+    @tile_rack.append(:A)
+    assert_equal 0, @tile_rack.number_of_tiles_needed
   end
 
 end
--- tile_rack.rb
+++ tile_rack.rb
@@ -1,11 +1,15 @@
 class TileRack < TileGroup
 
+	MAX_TILES_ON_RACK = 7
+
 	def number_of_tiles_needed
+		MAX_TILES_ON_RACK - @tiles.length
 	end
-	
+
 	def has_tiles_for?(text)
+		text.length - @tiles.length
 	end
-	
+
 	def remove_word(text)
 	end
 
