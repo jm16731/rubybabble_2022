818621ce3dff632b1cd497fddb08cc200da84b68
jmurrel1 <jmurrel1@cs6242-vm09.uwg.westga.edu>
2022-01-21 14:33:27 -0500

    notes

--- babble.rb
+++ babble.rb
@@ -1,10 +1,27 @@
+require 'spellchecker'
+require 'tempfile'
+
+#Spellchecker::check("WordToCheck")[0][:correct]
+
 class Babble
 
 	def initialize()
 	end
-	
+
 	def run()
+		#until ??
+
+  #  displays the letters in the tile rack
+  #  prompts the user to guess a word
+  #  if ":quit" is entered, displays "Thanks for playing, total score: 45" (replacing 45 with the actual total score).
+  #  if the word is not valid (i.e., fails spellcheck), displays "Not a valid word"
+  #  if the word is valid but the rack doesn't have the needed tiles, displays "Not enough tiles"
+  #  if the word is valid and has the proper tiles, displays "You made WORD for 10 points" (replacing WORD with the actual word guessed and the 10 with the actual number of points for that word)
+  #  At the end of every guess, regardless of whether it was right or not, the current total score is displayed.
+
 	end
+
+	private
 
 end
 Babble.new.run
