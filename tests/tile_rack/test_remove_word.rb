a00731dd7c0cb702d976fa7089093d55a9b4b4cf
jmurrel1 <jmurrel1@cs6242-vm09.uwg.westga.edu>
2022-01-21 13:47:00 -0500

    test_remove_word ready to test

--- tests/tile_rack/test_remove_word.rb
+++ tests/tile_rack/test_remove_word.rb
@@ -1,26 +1,66 @@
 require "minitest/autorun"
 require_relative "../../tile_rack.rb"
+require_relative "../../word.rb"
 
 class TileRack::TestRemoveWord < MiniTest::Test
 
   def setup
     @tile_rack = TileRack.new
+    @word_one = Word.new
+    @word_one.append(:C)
+    @word_one.append(:A)
+    @word_one.append(:T)
+
+    @word_two = Word.new
+    @word_two.append(:M)
+    @word_two.append(:A)
+    @word_two.append(:R)
+    @word_two.append(:R)
+    @word_two.append(:O)
+    @word_two.append(:W)
   end
 
   def test_can_remove_a_word_whose_letters_are_in_order_on_the_rack
-    skip "Test not yet implemented"
+    @tile_rack.append(:C)
+    @tile_rack.append(:A)
+    @tile_rack.append(:T)
+    assert_equal @word_one, @tile_rack.remove_word("CAT")
+    assert_equal 7, @tile_rack.number_of_tiles_needed
+    assert_equal false, @tile_rack.has_tiles_for?("CAT")
   end
 
   def test_can_remove_a_word_whose_letters_are_not_in_order_on_the_rack
-    skip "Test not yet implemented"
+    @tile_rack.append(:A)
+    @tile_rack.append(:C)
+    @tile_rack.append(:T)
+    assert_equal @word_one, @tile_rack.remove_word("CAT")
+    assert_equal 7, @tile_rack.number_of_tiles_needed
+    assert_equal false, @tile_rack.has_tiles_for?("CAT")
   end
 
   def test_can_remove_word_with_duplicate_letters
-    skip "Test not yet implemented"
+    @tile_rack.append(:M)
+    @tile_rack.append(:A)
+    @tile_rack.append(:R)
+    @tile_rack.append(:R)
+    @tile_rack.append(:O)
+    @tile_rack.append(:W)
+    assert_equal @word_two, @tile_rack.remove_word("MARROW")
+    assert_equal 7, @tile_rack.number_of_tiles_needed
+    assert_equal false, @tile_rack.has_tiles_for?("MARROW")
   end
 
   def test_can_remove_word_without_removing_unneeded_duplicate_letters
-    skip "Test not yet implemented"
+    @tile_rack.append(:M)
+    @tile_rack.append(:A)
+    @tile_rack.append(:R)
+    @tile_rack.append(:R)
+    @tile_rack.append(:O)
+    @tile_rack.append(:W)
+    @tile_rack.append(:R)
+    assert_equal @word_two, @tile_rack.remove_word("MARROW")
+    assert_equal 6, @tile_rack.number_of_tiles_needed
+    assert_equal false, @tile_rack.has_tiles_for?("MARROW")
   end
 
 end
