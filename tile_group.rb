f5f208ae60cb7949329bba6c1753e2326b527cd1
jmurrel1 <jmurrel1@cs6242-vm09.uwg.westga.edu>
2022-01-21 14:02:17 -0500

    attribute algorithm

--- tile_group.rb
+++ tile_group.rb
@@ -10,6 +10,9 @@ class TileGroup
 		@tiles.append(tile)
 	end
 
+	#delete only the first occurence of a value in the array of tiles
+	#algorithm by Weston Ganger
+	#https://westonganger.com/posts/delete-the-first-instance-of-value-from-array-in-ruby
 	def remove(tile)
 		@tiles.delete_at(@tiles.index(tile) || @tiles.length)
 	end
